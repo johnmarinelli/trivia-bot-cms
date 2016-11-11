409694c5f8b734d2dbbce948490f9243047ba90a
johnmarinelli <john.marinelli.dev@gmail.com>
2016-11-10 21:06:03 -0800

    hardcode uri

--- src/trivia_cms/db/config.clj
+++ src/trivia_cms/db/config.clj
@@ -6,10 +6,11 @@
 
 (def database (env :database-name))
 (def db-handle 
-  (:db (mg/connect-via-uri 
-        (clojure.string/join 
+  (:db (mg/connect-via-uri "mongodb://heroku_np83q2zj:9o8vhhj4nchpddr3ancrh0hlen@ds025389.mlab.com:25389/heroku_np83q2zj")))
+
+(comment(clojure.string/join 
          "/" 
-         (map env [:mongodb-uri-host :database-name])))))
+         (map env [:mongodb-uri-host :database-name])))
 
 (def quizzes-collection-name "quizzes")
 (def questions-collection-name "questions")
