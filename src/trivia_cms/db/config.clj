f321c5723ffa2d7c9b8f75e0889e7acb2e3924e8
John Marinelli <john.marinelli.dev@gmail.com>
2016-12-13 01:09:28 -0800

    add connection

--- src/trivia_cms/db/config.clj
+++ src/trivia_cms/db/config.clj
@@ -4,6 +4,16 @@
             [environ.core :refer [env]])
   (:import [com.mongodb MongoOptions ServerAddress]))
 
+(def conn 
+  (if (nil? (System/getenv "MONGODB_URI")) 
+    (mg/connect 
+     {:host 
+      (or (get (System/getenv) "MONGO_TRIVIA_BOT_HOST") "127.0.0.1") 
+      :port 
+      (Integer. (or (get (System/getenv) "MONGO_TRIVIA_BOT_PORT") "27017"))})
+    (mg/connect 
+     (:conn (mg/connect-via-uri (System/getenv "MONGODB_URI"))))))
+
 (def db-handle 
   (:db 
    (mg/connect-via-uri 
